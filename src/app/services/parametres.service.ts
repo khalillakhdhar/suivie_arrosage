343fd9a17ef8cb34e812bacb9a34e7fbc09ca63e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-28 16:47:03 +0100

    Update parametres.service.ts

--- src/app/services/parametres.service.ts
+++ src/app/services/parametres.service.ts
@@ -1,9 +1,31 @@
+  
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ParametresService {
+export class Parameterservice {
 
-  constructor() { }
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+
+  create_NewParameters(record) {
+    return this.firestore.collection('Parameters').add(record);
+  }
+
+  read_Parameters() {
+    return this.firestore.collection('Parameters').snapshotChanges();
+  }
+
+  update_Parameters(recordID, record) {
+    this.firestore.doc('Parameters/' + recordID).update(record);
+    console.log('updated');
+  }
+
+  delete_Parameters(record_id) {
+    this.firestore.doc('Parameters/' + record_id).delete();
+  }
 }
