e3dea30e7541ac1e15a73d34ad8e187384c21d0e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-11 10:57:50 +0100

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Utilisateur } from 'app/classes/utilisateur';
+import { UserService } from 'app/services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +10,34 @@ import { Utilisateur } from 'app/classes/utilisateur';
 export class DashboardComponent implements OnInit {
   user:Utilisateur;
   users:Utilisateur[];
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   ngOnInit() {
       
   }
-
+  read()
+  {
+    this.userService.read_Users().subscribe(data => {
+  
+      this.users = data.map(e => {
+        return {
+         id: e.payload.doc.id,
+  
+         nom: e.payload.doc.data()["nom"],
+         tel: e.payload.doc.data()["tel"],
+         grade: e.payload.doc.data()["grade"],
+         login: e.payload.doc.data()["login"],
+         mdp: e.payload.doc.data()["mdp"],
+         adresse: e.payload.doc.data()["adresse"],
+  
+  
+  
+        };
+      });
+      console.log(this.users);
+  
+    });
+  
+  
+  }
 }
